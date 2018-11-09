1efed14f10354024e473788e279aeebcf1519e57
cristian <crist.1707@hotmail.com>
2018-11-09 12:40:47 -0500

    test

---
+++ Assets/My assets/Scripts/AI/AIController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AIController : MonoBehaviour {
+
+    [SerializeField]
+    private NavMeshAgent pathManager;
+    public Transform t;
+
+
+	// Use this for initialization
+	void Start () {
+        pathManager = GetComponent<NavMeshAgent>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        pathManager.SetDestination(t.position);
+	}
+}
